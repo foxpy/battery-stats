3a8124095829d611c4468e46eaabe6230a92a3fa
Murad <muradkarammaev@gmail.com>
2020-07-10 15:27:13 +0300

    process custom samples
    I have no clue why would it be necessary,
    but homework is homework :v

--- src/process-stats/bin/main.rs
+++ src/process-stats/bin/main.rs
@@ -1,6 +1,6 @@
-use std::{env, process};
-use std::fs::File;
 use getopts::Options;
+use std::fs::File;
+use std::{env, process};
 
 fn print_help(program_name: &str, opts: Options) -> ! {
     let brief = format!("Usage: {} input.csv", program_name);
@@ -9,9 +9,11 @@ fn print_help(program_name: &str, opts: Options) -> ! {
 }
 
 fn read_input_file(file: &str) -> std::io::Result<Vec<f64>> {
-    let mut measures = vec!();
+    let mut measures = vec![];
     let file = File::open(file)?;
-    let mut reader = csv::ReaderBuilder::new().has_headers(true).from_reader(file);
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(file);
     for i in reader.records() {
         let record = i?;
         let power: f64 = record[1].parse().unwrap();
@@ -24,15 +26,14 @@ fn process_data(input: &Vec<f64>) {
     let mut sorted = input.clone();
     sorted.sort_by(|a, b| a.partial_cmp(b).unwrap());
     let average = input.iter().sum::<f64>() / input.len() as f64;
-    let dispersion = (input.iter().map(|x| x.powf(2.0)).sum::<f64>() / input.len() as f64) - average.powf(2.0);
+    let dispersion =
+        (input.iter().map(|x| x.powf(2.0)).sum::<f64>() / input.len() as f64) - average.powf(2.0);
     let stddev = dispersion.sqrt();
-    let each_second = input.into_iter().enumerate().filter(|(i, _)| i % 2 == 1).map(|(_, v)| v).collect::<Vec<_>>();
     println!("Генеральная совокупность: {:?}", input);
     println!("Вариационный ряд: {:?}", sorted);
     println!("Среднее значение: {:.5}", average);
     println!("Дисперсия: {:.5}", dispersion);
     println!("Стандартное отклонение: {:.5}", stddev);
-    println!("Выборка (каждый второй): {:?}", each_second);
 }
 
 fn main() {
@@ -43,10 +44,28 @@ fn main() {
     if matches.opt_present("h") {
         print_help(&args[0], opts);
     }
-
     if matches.free.is_empty() {
         panic!("No input file specified");
     }
+
     let measures = read_input_file(&matches.free[0]).unwrap();
     process_data(&measures);
+    println!("\nВыборка [каждый второй]:");
+    let each_second = measures
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 1)
+        .map(|(_, v)| v)
+        .collect::<Vec<_>>();
+    process_data(&each_second);
+    println!("\nВыборка [каждый пятый]:");
+    let each_fifth = measures
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|(i, _)| i % 5 == 1)
+        .map(|(_, v)| v)
+        .collect::<Vec<_>>();
+    process_data(&each_fifth);
 }
