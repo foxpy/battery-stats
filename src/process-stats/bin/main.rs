662f2854bba57bbf86befb8076fa62528d871a94
Murad <muradkarammaev@gmail.com>
2020-07-11 09:13:11 +0300

    start working on plotting

--- src/process-stats/bin/main.rs
+++ src/process-stats/bin/main.rs
@@ -2,6 +2,20 @@ use getopts::Options;
 use std::fs::File;
 use std::{env, fmt, process};
 
+#[derive(PartialEq, Eq, Hash)]
+struct Decimal {
+    val: u64,
+    fract: u8,
+}
+
+impl Decimal {
+    fn new(x: f64) -> Decimal {
+        let val = x as u64;
+        let fract = ((x * 100.0) as u64 % 100) as u8;
+        Decimal{val, fract}
+    }
+}
+
 #[derive(Default)]
 struct StatisticalPopulation {
     statistical_population: Vec<f64>,
@@ -33,6 +47,14 @@ impl StatisticalPopulation {
         sp.corrected_stddev = sp.corrected_dispersion.sqrt();
         sp
     }
+
+    fn plot_frequency_range(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+
+    fn plot_histogram(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
 }
 
 impl fmt::Display for StatisticalPopulation {
@@ -60,7 +82,7 @@ impl fmt::Display for StatisticalPopulation {
 }
 
 fn print_help(program_name: &str, opts: Options) -> ! {
-    let brief = format!("Usage: {} input.csv", program_name);
+    let brief = format!("Usage: {} [options] input.csv", program_name);
     print!("{}", opts.usage(&brief));
     process::exit(0);
 }
@@ -94,9 +116,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this message");
+    opts.optopt("d", "output-directory", "Graph output folder", "DIR");
     let matches = opts.parse(&args[1..]).unwrap();
     if matches.opt_present("h") {
         print_help(&args[0], opts);
+    }
+    if matches.opt_present("d") {
+        env::set_current_dir(matches.opt_str("d").unwrap()).unwrap();
     }
     if matches.free.is_empty() {
         panic!("No input file specified");
